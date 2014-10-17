31db7d30245ceab154a2e89e25f9a599b1e138d0
Will Bradley <williambbradley@gmail.com>
2014-10-17 00:58:10 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='logit',
       version='0.1',
-      py_modules=['logit', 'utils'],
+      description='A log for humans',
+      author='William Bradley',
+      author_email='williambbradley@gmail.com',
       )
