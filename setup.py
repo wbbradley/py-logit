1e41530c1281bde980b4e10e6a4e65d1410036a5
Will Bradley <williambbradley@gmail.com>
2014-11-30 15:38:39 -0800

    free this work

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     description='A private log for humans',
     author='Will Bradley',
     author_email='williambbradley@gmail.com',
+    license='CC0',
     url='https://github.com/wbbradley/logit',
     packages=['logit'],
     entry_points={'console_scripts': [
