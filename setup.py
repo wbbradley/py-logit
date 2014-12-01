824f93462d7467c0657a4cdb165e21b4bdb6074c
Will Bradley <williambbradley@gmail.com>
2014-11-30 23:43:00 -0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='logit-safe',
-    version='0.6',
+    version='0.7',
     description='A private log for humans',
     author='Will Bradley',
     author_email='williambbradley@gmail.com',
