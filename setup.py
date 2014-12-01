01e7bc0a9fad6c952477d00d6c3fd6f0da788f6b
Will Bradley <williambbradley@gmail.com>
2014-11-30 23:58:30 -0800

    bump version - fix distribution bug

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='logit-safe',
-    version='0.7',
+    version='0.7.1',
     description='A private log for humans',
     author='Will Bradley',
     author_email='williambbradley@gmail.com',
     license='CC0',
     url='https://github.com/wbbradley/logit',
     packages=['logit'],
-    install_requires=['boto', 'PyYAML'],
+    install_requires=['pycrypto', 'boto', 'PyYAML'],
     entry_points={'console_scripts': [
         'logit = logit.main:main',
     ]},
