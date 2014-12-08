9957ceab13546afa2a3affed6a9a878a1d88f38f
Will Bradley <williambbradley@gmail.com>
2014-12-07 21:47:58 -0800

    standard find_packages

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='logit',
-    version='0.7.3',
+    version='0.7.4',
     description='A private log for humans',
     author='Will Bradley',
     author_email='williambbradley@gmail.com',
     license='CC0',
     url='https://github.com/wbbradley/logit',
-    packages=['logit'],
-    install_requires=['pycrypto', 'boto', 'PyYAML',
-                      'attrdict'],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"]),
+    install_requires=['pycrypto', 'boto', 'PyYAML', 'attrdict'],
     entry_points={'console_scripts': [
         'logit = logit.main:main',
     ]},
